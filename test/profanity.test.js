817481ec1884fc734d374ef49da1a8a587d66ee6
tancredi <tancredi.trugenberger@gmail.com>
2014-06-03 12:27:09 +0100

    Update profanity.test.js

--- test/profanity.test.js
+++ test/profanity.test.js
@@ -1,6 +1,6 @@
 /*
 Copyright (C) 2014 Kano Computing Ltd.
-License: http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+License: http://opensource.org/licenses/MIT The MIT License (MIT)
 */
 
 var should = require('should'),
